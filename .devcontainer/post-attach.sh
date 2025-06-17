e025f33ff6d04c3ea842b78f0cfbf864385563a7
James Cape <jamescape777@gmail.com>
2025-06-17 22:34:30 +0000

    build: binstall, sccache, pre-commit fix
    - Installs `cargo-binstall` via GH release
    - Uses `cargo-binstall` to install tools into the devcontainer
    - Adds and configures `sccache`
    - Ignores the LICENSE file in pre-commit hooks
    - Configures the pre-commit hooks to use the nodejs version the devcontainer already uses

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,10 +3,14 @@
 mkdir -p /workspaces/iso17442/.cache/cargo
 ln -sf /usr/local/cargo/bin /workspaces/iso17442/.cache/cargo/
 
-cargo install -q cargo-semver-checks
-cargo install -q release-plz
+RUSTC_WRAPPER_save="$RUSTC_WRAPPER"
+unset RUSTC_WRAPPER
+cargo binstall -q -y --force sccache
+export RUSTC_WRAPPER="$RUST_WRAPPER_save"
+
+cargo binstall -q -y --force cargo-semver-checks
+cargo binstall -q -y --force release-plz
 
 pushd /workspaces/iso17442 >/dev/null
 pre-commit install >/dev/null
 popd >/dev/null
-
