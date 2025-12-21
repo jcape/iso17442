63e6159add8bf4ff5213497093ff139f8b4cacc8
James Cape <jamescape777@pm.me>
2025-12-21 16:34:41 +0000

    build(dev): remove sccache, use prek not pre-commit

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,16 +3,12 @@
 mkdir -p /workspaces/iso17442/.cache/cargo
 ln -sf /usr/local/cargo/bin /workspaces/iso17442/.cache/cargo/
 
-RUSTC_WRAPPER_save="$RUSTC_WRAPPER"
-unset RUSTC_WRAPPER
-cargo binstall -q -y --force sccache
-export RUSTC_WRAPPER="$RUST_WRAPPER_save"
-
+cargo binstall -q -y --force prek
 cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
 cargo binstall -q -y --force cargo-semver-checks
 cargo binstall -q -y --force release-plz
 
 pushd /workspaces/iso17442 >/dev/null
-pre-commit install >/dev/null
+prek install >/dev/null
 popd >/dev/null
