42743c17c53f7f2ce2fd984e208596dae97bbd8e
James Cape <jamescape777@pm.me>
2026-01-11 18:07:14 +0000

    ci: switch to nextest, upload test results

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -7,6 +7,7 @@ cargo binstall -q -y --force prek
 cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
 cargo binstall -q -y --force cargo-semver-checks
+cargo binstall -q -y --force cargo-nextest
 cargo binstall -q -y --force release-plz
 
 pushd /workspaces/iso17442 >/dev/null
